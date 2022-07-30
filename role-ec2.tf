1294bd684c3fc7918d7494a1f336e4f26b22d256
oscar <oscarkip2301@outlook.com>
2022-07-30 14:01:51 -0700

    12 change

--- role-ec2.tf
+++ role-ec2.tf
@@ -1,24 +1,24 @@
 resource "aws_iam_policy" "ec2_policy" {
-  name        = "K8s-policy"
+  name        = "K8s-exam-policy"
   description = "My terraform server policy"
   policy = "${file("policy.json")}"
 }
 
 
 
-resource "aws_iam_role" "ec2_role" {
-  name = "ec2_k8s_role"
+resource "aws_iam_role" "k8s_exam_ec2_role" {
+  name = "ec2_exam_k8s_role"
   assume_role_policy = "${file("assume_policy.json")}"
 }
 
 resource "aws_iam_policy_attachment" "k8s_policy_attach" {
-  name = "terraform attachment"
-  roles = [aws_iam_role.ec2_role.name]
+  name = "kubernetes attachment"
+  roles = [aws_iam_role.k8s_exam_ec2_role.name]
   policy_arn = aws_iam_policy.ec2_policy.arn
   
 }
 
 resource "aws_iam_instance_profile" "ec2_proflile" {
-  name  = "ec2_profile"
-  role = aws_iam_role.ec2_role.name
+  name  = "ec2_k8s_profile"
+  role = aws_iam_role.k8s_exam_ec2_role.name
 }
