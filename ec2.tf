22db5654d5594f4f6955516e9926fdb2189d26d2
oscar <oscarkip2301@outlook.com>
2022-07-30 00:07:50 -0700

    Second Commit

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "master1-k8s" {
   iam_instance_profile   = aws_iam_instance_profile.ec2_proflile.name
   vpc_security_group_ids = [aws_security_group.main_web_sg.id]
   key_name               = var.keypair
-  user_data = "${file("master-node.sh")}"
+  user_data = "${file("user-data.sh")}"
   tags = {
     Name = var.master-name
     Env  = var.env
@@ -17,7 +17,7 @@ resource "aws_instance" "node1-k8s" {
   instance_type = var.node_instance_type
   vpc_security_group_ids = [aws_security_group.main_web_sg.id]
   key_name      = var.keypair
-  user_data = "${file("master-node.sh")}"
+  user_data = "${file("user-data.sh")}"
   tags = {
     Name = var.node1-name
     Env  = var.env
@@ -29,7 +29,7 @@ resource "aws_instance" "node2-k8s" {
   instance_type = var.node_instance_type
   vpc_security_group_ids = [aws_security_group.main_web_sg.id]
   key_name      = var.keypair
-  user_data = "${file("master-node.sh")}"
+  user_data = "${file("user-data.sh")}"
   tags = {
     Name = var.node2-name
     Env  = var.env
---
+++ user-data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo swapoff -a
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update -y
+
+sudo apt-get install -y kubelet kubeadm kubectl
+
+sudo apt-mark hold kubelet kubeadm kubectl
