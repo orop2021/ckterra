197039208a9f825991992e01a2813aac8ffb485a
oscar <oscarkip2301@outlook.com>
2022-07-30 12:03:08 -0700

    fith commit

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir cka-exam
+cd cka-exam
+git clone https://github.com/orop2021/ckterra.git
+terraform fmt
+terraform init
+terraform validate
+terraform plan
+terraform apply --auto-approve
+echo "Terraform success"
