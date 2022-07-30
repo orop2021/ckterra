01c9ca738da2706e4c6b4c580181c0d517620ff1
oscar <oscarkip2301@outlook.com>
2022-07-30 10:41:30 -0700

    third commit

--- role-ec2.tf
+++ role-ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy" "ec2_policy" {
-  name        = "${random_pet.pet_name.id}-policy"
+  name        = "K8s-policy"
   description = "My terraform server policy"
   role = "${aws_iam_role.ec2_role.id}"
   policy = "${file("policy.json")}"
@@ -10,13 +10,6 @@ resource "aws_iam_policy" "ec2_policy" {
 resource "aws_iam_role" "ec2_role" {
   name = "ec2_k8s_role"
   assume_role_policy = "${file("assume_policy.json")}"
-}
-
-resource "aws_iam_policy_attachment" "ec2_policy_attach" {
-  name = "ec2_attachment"
-  roles = [aws_iam_role.ec2_role.name]
-  
-  
 }
 
 resource "aws_iam_instance_profile" "ec2_proflile" {
