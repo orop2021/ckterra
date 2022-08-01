9848de9cd424f7436e51823838fc18d94ec70564
oscar <oscarkip2301@outlook.com>
2022-07-31 20:15:33 -0700

    17 chnage

--- start.sh
+++ start.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-mkdir cka-exam
-cd cka-exam
-git clone https://github.com/orop2021/ckterra.git
-terraform fmt
-terraform init
-terraform validate
-terraform plan
-terraform apply --auto-approve
-echo "Terraform success"
-echo "goodluck"
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
--- user-data.sh
+++ user-data.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo swapoff -a
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-cache policy docker-ce
-apt-cache policy docker-ce
-sudo apt install docker-ce  -y
-sudo systemctl start docker
-sudo systemctl enable docker
+#sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+#apt-cache policy docker-ce
+#apt-cache policy docker-ce
+#sudo apt install docker-ce  -y
+#sudo systemctl start docker
+#sudo systemctl enable docker
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
@@ -20,3 +20,21 @@ sudo apt-get update -y
 sudo apt-get install -y kubelet kubeadm kubectl
 
 sudo apt-mark hold kubelet kubeadm kubectl
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
